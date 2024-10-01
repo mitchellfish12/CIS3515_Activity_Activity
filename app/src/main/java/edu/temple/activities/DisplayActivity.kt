310c30dc6446a067dabc62157ea89c3a09e1d5e9
Mitchell Fishman <tuq42483@temple.edu>
2024-10-01 15:34:20 -0400

    First commit-work I did on Tuesday's class w/ my group, will work to resolve errors before thursday's class

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Contacts.SettingsColumns.KEY
 import android.widget.TextView
 
 class DisplayActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            textSize = intent.getFloatExtra(KEY, defaultValue: 22f)
         }
 
 
